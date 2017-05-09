b5ac6617a9f47b8ae6ef047981f0dd514d0ce076
Aimee Bachari <aimeebachari@gmail.com>
2017-05-09 11:55:34 -0400

    Created foundation column for playlists and passed data in.

--- react-marathon/src/components/App.js
+++ react-marathon/src/components/App.js
@@ -6,18 +6,23 @@ class App extends React.Component {
   }
 
   render() {
-    let data = this.props.data
+    let data = this.props.data;
 
     let selectedPlaylistSongIds = data.playlists[this.props.data.selectedPlaylistId-1].songs;
 
     let filterById = (obj) => {
       return selectedPlaylistSongIds.includes(obj.id);
-    }
+    };
 
     let selectedPlaylistSongs = data.songs.filter(filterById);
 
     return (
       <div className="App row">
+        <div className="column medium-6">
+          <h1>Playlist Portion<h1>
+          <PlaylistCollection
+            playLists={data.playlists}
+          />
       </div>
     );
   }
